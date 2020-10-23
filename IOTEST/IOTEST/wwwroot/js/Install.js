62bfc7a4d542c6d6bd7cc118ff78f96ded409637
Андрей Попов <andreypopov2628007@gmail.com>
2020-10-24 00:05:11 +0300

    Установка почти  готова

--- IOTEST/IOTEST/wwwroot/js/Install.js
+++ IOTEST/IOTEST/wwwroot/js/Install.js
@@ -1,7 +1,23 @@
 ﻿var app = new Vue({
     el: "#app",
     data: {
-        seen: true
+        seen: true,
+        deferredPrompt: null
+    },
+    methods: {
+        InstallClick() {
+            this.deferredPrompt.prompt();
+        }
+    },
+    created() {
+        window.addEventListener("beforeinstallprompt", e => {
+            e.preventDefault();
+            this.deferredPrompt = e;
+        });
+        window.addEventListener("appinstalled", () => {
+            console.log("instaled")
+            this.deferredPrompt = null;
+        });
     },
     mounted() {
       
