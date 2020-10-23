195592f2608abfc33f197355667a289457dfda72
Андрей Попов <andreypopov2628007@gmail.com>
2020-10-23 12:45:33 +0300

    Фикс кликабельности фона

--- IOTEST/IOTEST/wwwroot/VueComponents/InstallCompoonents.js
+++ IOTEST/IOTEST/wwwroot/VueComponents/InstallCompoonents.js
@@ -6,8 +6,7 @@
             particleColor: '#0fffff',
             background: "#0f0f0f",
             interactive: true,
-            density: 9000,
-
+            density: 6000,
         };
         var particleCanvas = new ParticleNetwork(canvasDiv, options);
     },
