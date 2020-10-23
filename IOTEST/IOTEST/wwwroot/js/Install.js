4a97eaf3c09466dd9df07197dc086356241d5b11
Андрей Попов <andreypopov2628007@gmail.com>
2020-10-23 11:52:36 +0300

    Добавление библиотеки красивого фона и подключение библиотек в install

---
+++ IOTEST/IOTEST/wwwroot/js/Install.js
@@ -0,0 +1,8 @@
+﻿var canvasDiv = document.getElementById('particle-canvas');
+var options = {
+    particleColor: '#0fffff',
+    background: "#0f0f0f",
+    interactive: true,
+    density: 9000,
+};
+var particleCanvas = new ParticleNetwork(canvasDiv, options);
