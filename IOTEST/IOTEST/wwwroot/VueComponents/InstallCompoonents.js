d536443d5335e0accfacc7afb64d27a51736a732
Андрей Попов <andreypopov2628007@gmail.com>
2020-10-23 12:22:39 +0300

    Фон успешно заработал

--- IOTEST/IOTEST/wwwroot/VueComponents/InstallCompoonents.js
+++ IOTEST/IOTEST/wwwroot/VueComponents/InstallCompoonents.js
@@ -1,6 +1,14 @@
 ﻿Vue.component('backganim', {
     template: '<div id="canvAn1"></div>',
-    created: function () {
-      
+    mounted: function () {
+        var canvasDiv = document.getElementById('canvAn1');
+        var options = {
+            particleColor: '#0fffff',
+            background: "#0f0f0f",
+            interactive: true,
+            density: 9000,
+
+        };
+        var particleCanvas = new ParticleNetwork(canvasDiv, options);
     },
 })
--- IOTEST/IOTEST/wwwroot/js/Install.js
+++ IOTEST/IOTEST/wwwroot/js/Install.js
@@ -8,12 +8,3 @@
     }
 })
 
-var canvasDiv = document.getElementById('canvAn1');
-var options = {
-    particleColor: '#0fffff',
-    background: "#0f0f0f",
-    interactive: true,
-    density: 9000,
-
-};
-var particleCanvas = new ParticleNetwork(canvasDiv, options);
