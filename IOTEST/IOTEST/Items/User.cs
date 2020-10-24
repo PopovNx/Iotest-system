b4e377f8c280162e3219c5e7405534affb2d8b9e
Андрей Попов <andreypopov2628007@gmail.com>
2020-10-24 12:23:29 +0300

    Добавлен класс User

---
+++ IOTEST/IOTEST/Items/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOTEST.Items
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string FamilyName { get; set; }
+        public string Image { get; set; }
+        public string Gmail { get; set; }
+        public string Token { get; set; }
+
+    }
+}
