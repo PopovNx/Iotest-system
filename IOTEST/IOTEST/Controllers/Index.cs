a245a34f084f3ca3cac1568ce850e5545190177f
Андрей Попов <andreypopov2628007@gmail.com>
2020-10-28 15:42:14 +0200

    Добавление хедера с именем

--- IOTEST/IOTEST/Controllers/Index.cs
+++ IOTEST/IOTEST/Controllers/Index.cs
@@ -11,12 +11,14 @@ namespace IOTEST.Controllers
     {
         public IActionResult Index()
         {
-
+            DataControl control = new DataControl(HttpContext.Request.Cookies);
             ViewData.Add("Title", "IOTEST");
             ViewData.Add("ParalaxOn", true);
             ViewData.Add("CSS", new List<string> { "css/Index.css" });
             ViewData.Add("JSU", new List<string> { "VueComponents/InstallCompoonents.js" });
             ViewData.Add("JSD", new List<string> { "js/Index.js" });
+            ViewData.Add("User", control.UserData);
+
             return View("Index");
         }
     }
