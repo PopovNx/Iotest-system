1beca28f7d7352ed7cad1016c104072b681d203c
Андрей Попов <andreypopov2628007@gmail.com>
2020-11-08 12:21:58 +0200

    Оптимизация поля, добавление структуры ООП

--- IOTEST/IOTEST/wwwroot/TestItems/Classes.js
+++ IOTEST/IOTEST/wwwroot/TestItems/Classes.js
@@ -1,4 +1,162 @@
-﻿class DragableObject {
+﻿
+class SavedMap {
+    constructor(objs, trgs) {
+        this.Objects = objs;
+        this.Triggers = trgs;
+
+    }
+    Objects;
+    Triggers;
+
+}
+SavedMap.sObject = class {
+    constructor(st, gr, ty, pos, vari, id, w) {
+        this.State = st;
+        this.Group = gr;
+        this.Type = ty;
+        this.Position = pos;
+        this.Variant = vari;
+        this.Id = id;
+        this.Weight = w;
+    }
+    State;
+    Group;
+    Type;
+    Position;
+    Variant;
+    Id;
+    Weight;
+}
+SavedMap.sTrigger = class {
+
+}
+SavedMap.sObject.PositionT = class {
+    constructor(x, y, z, s, r) {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+        this.Size = s;
+        this.Rotation = r;
+    }
+    X;
+    Y;
+    Z;
+    Size;
+    Rotation;
+}
+class VisualMap {
+    constructor(smap) {
+        smap.Objects.sort(function (x, y) { return x.Position.Z < y.Position.Z ? -1 : 1 })
+        smap.Objects.forEach((e) => {
+            switch (e.Group) {
+                case "Electrons":
+                    this.Objects.push(new ElectronsObjects(parseInt(e.Position.Size) / 100, parseInt(e.Position.X), parseInt(e.Position.Y), parseInt(e.Position.Rotation), ElectronsObjects.Types[e.Type], e.State == "Dynamic" ? true : false, e.Variant));
+                    break;
+                default:
+                    console.error(e);
+            }
+        });
+
+    }
+    Work() {
+
+
+
+
+    }
+    Objects = [];
+    Triggers = [];
+}
+class VisualTest {
+    constructor(smap, display) {
+        this.Canvas = display;
+        this.VDisplay = new PIXI.Application({ width: 750, height: 500, view: this.Canvas , backgroundColor: 0xf0f0f0, antialias: true, })
+        this.VDisplayContainer = new PIXI.Container();
+        this.VDisplay.stage.addChild(this.VDisplayContainer);
+        this.Vmap = new VisualMap(smap);
+        this.Vmap.Objects.forEach((e) => this.VDisplayContainer.addChild(e.sprite));
+        this.Vmap.Triggers.forEach((e) => container.addChild(e.graphics));
+        this.VDisplay.ticker.add((delta) => this.TestWorker(delta));
+        window.addEventListener('resize', () => { this.resize(this); });
+        this.resize(this);
+    }
+    TrigerWorker() {
+        this.Vmap.Triggers.forEach((e) => {
+            e.Draw();
+            var elementcount = 0;
+            this.Vmap.Elements.forEach((el) => {
+                if (e.magnetic) {
+                    if (Funcs.pointInPoly(e.VectorArray, el.sprite.x, el.sprite.y)) {
+                        elementcount++;
+                    }
+                }
+                if (el.sprite.dragging) {
+                    if (e.magnetic && Funcs.pointInPoly(e.VectorArray, el.sprite.hasposx, el.sprite.hasposy)) {
+                        el.sprite.OnTrigger = true;
+                        el.sprite.x = e.x;
+                        el.sprite.y = e.y;
+                    } else el.sprite.OnTrigger = false;
+                }
+
+            })
+            e.elementcount = elementcount;
+            // console.log(e.elementcount);
+        });
+    }
+    TestWorker(delta) {
+        this.TrigerWorker();
+    }
+    VDisplay;
+    VDisplayContainer;
+    Vmap;
+    Canvas;
+    Funcs = {
+        getPointOfIntersection(startX1, startY1, endX1, endY1, startX2, startY2, endX2, endY2) {
+            var d = (startX1 - endX1) * (endY2 - startY2) - (startY1 - endY1) * (endX2 - startX2);
+            var da = (startX1 - startX2) * (endY2 - startY2) - (startY1 - startY2) * (endX2 - startX2);
+            var db = (startX1 - endX1) * (startY1 - startY2) - (startY1 - endY1) * (startX1 - startX2);
+
+            var ta = da / d;
+            var tb = db / d;
+
+            if (ta >= 0 && ta <= 1 && tb >= 0 && tb <= 1) {
+                var dx = startX1 + ta * (endX1 - startX1);
+                var dy = startY1 + ta * (endY1 - startY1);
+                return [dx, dy];
+            }
+            return [-100, -100];
+        },
+        pointInPoly(poly, pointX, pointY) {
+            var i, j, c = false;
+            var polyCords = [[poly[0], poly[1]], [poly[2], poly[3]], [poly[4], poly[5]], [poly[6], poly[7]]];
+            for (i = 0, j = polyCords.length - 1; i < polyCords.length; j = i++) {
+
+                if (((polyCords[i][1] > pointY) != (polyCords[j][1] > pointY)) && (pointX < (polyCords[j][0] - polyCords[i][0]) * (pointY - polyCords[i][1]) / (polyCords[j][1] - polyCords[i][1]) + polyCords[i][0])) {
+                    c = !c;
+                }
+
+            }
+
+            return c;
+        }
+    }
+    resize(t) {
+        let TestWidth = document.getElementById("testMain").clientWidth;
+        t.VDisplay.renderer.resize(TestWidth, TestWidth / 3 * 2);
+        t.VDisplay.stage.scale.set(TestWidth / 900, TestWidth / 900);
+    }
+    destroy() {
+        console.log("destroy");
+        this.VDisplay.destroy();
+        this.VDisplay = undefined;
+        this.Vmap = undefined;
+        this.Funcs = undefined;
+        this.resize = undefined;
+        this.VDisplayContainer = undefined;
+    }
+}
+
+class DragableObject {
     static NullTexture = 'Prefabs/Shared/Null.png';
     constructor(img, scale, x, y, r, cc = true, drag = false) {
         this.texture = PIXI.Texture.from(img);
@@ -71,8 +229,6 @@ class ElectronsObjects extends DragableObject {
     constructor(scale, x, y, r, type, isdragable, varitant) {
         var texture = type.textures[varitant];
         if (texture === undefined) texture = DragableObject.NullTexture;
-
-
         super(texture, scale, x, y, r, true, isdragable);
         this.type = type;
     }
--- IOTEST/IOTEST/wwwroot/js/Test.js
+++ IOTEST/IOTEST/wwwroot/js/Test.js
@@ -1,130 +1,23 @@
-﻿var Map = {
-    Objects: [
-
-        { type: "Static.Electrons.Wire", variant: 1, position: { X: 600, Y: 400, Z: -1, S: 20, R: -90 } },
-        { type: "Static.Electrons.Wire", variant: 1, position: { X: 100, Y: 400, Z: -1, S: 20, R: 0 } },
-        { type: "Static.Electrons.Wire", variant: 0, position: { X: 400, Y: 400, Z: -1, S: 20, R: 0 } },
-        { type: "Static.Electrons.Wire", variant: 0, position: { X: 300, Y: 400, Z: -1, S: 20, R: 0 } },
-        { type: "Static.Electrons.Wire", variant: 0, position: { X: 600, Y: 300, Z: -1, S: 20, R: 90 } },
-        { type: "Static.Electrons.Wire", variant: 0, position: { X: 100, Y: 300, Z: -1, S: 20, R: 90 } },
-        { type: "Static.Electrons.Wire", variant: 0, position: { X: 100, Y: 200, Z: -1, S: 20, R: 90 } },
-        { type: "Static.Electrons.Wire", variant: 0, position: { X: 600, Y: 200, Z: -1, S: 20, R: 90 } },
-        { type: "Static.Electrons.Wire", variant: 1, position: { X: 600, Y: 100, Z: -1, S: 20, R: -180 } },
-        { type: "Static.Electrons.Wire", variant: 1, position: { X: 100, Y: 100, Z: -1, S: 20, R: 90 } },
-        { type: "Static.Electrons.Wire", variant: 0, position: { X: 500, Y: 100, Z: -1, S: 20, R: 0 } },
-        { type: "Static.Electrons.Wire", variant: 0, position: { X: 400, Y: 100, Z: -1, S: 20, R: 0 } },
-        { type: "Static.Electrons.Wire", variant: 0, position: { X: 200, Y: 100, Z: -1, S: 20, R: 0 } },
-        { type: "Static.Trigger.Visual", variant: 0, position: { X: 300, Y: 100, Z: -1, S: 80, }, magnetic: true },
-        { type: "Static.Trigger.Visual", variant: 0, position: { X: 200, Y: 400, Z: -1, S: 80 }, magnetic: true },
-        { type: "Static.Trigger.Visual", variant: 0, position: { X: 500, Y: 400, Z: -1, S: 80 }, magnetic: true },
-        { type: "Dynamic.Electrons.Led", variant: 0, position: { X: 300, Y: 300, Z: 2, S: 20, R: 0 } },
-        { type: "Dynamic.Electrons.Reostat", variant: 0, position: { X: 300, Y: 300, Z: 1, S: 20, R: 0 } },
-        { type: "Dynamic.Electrons.Battery", variant: 0, position: { X: 300, Y: 300, Z: 3, S: 20, R: 0 } },
-
-
+﻿'use strict';
+var Smap = new SavedMap(
+    [
+        new SavedMap.sObject("Dynamic", "Electrons", "Led",     new SavedMap.sObject.PositionT(100, 300, 1, 20, 45),0, 0, 1),
+        new SavedMap.sObject("Dynamic", "Electrons", "Battery", new SavedMap.sObject.PositionT(400, 200, 0, 40, 0), 0, 1, 1),
+    ],
+    [
 
     ]
-}
-var Elements = [];
-var Triggers = [];
-Map.Objects.sort(function (x, y) { return x.position.Z < y.position.Z ? -1 : 1 })
-Map.Objects.forEach((e) => {
-    var elST = e.type.split(".")[0];
-    var elClass = e.type.split(".")[1];
-    var elType = e.type.split(".")[2];
-    switch (elClass) {
-        case "Electrons":
-            Elements.push(new ElectronsObjects(parseInt(e.position.S) / 100, parseInt(e.position.X), parseInt(e.position.Y), parseInt(e.position.R), ElectronsObjects.Types[elType], elST == "Dynamic" ? true : false, e.variant));
-            break;
-        case "Trigger":
-            Triggers.push(new Trigger(parseInt(e.position.S), parseInt(e.position.X), parseInt(e.position.Y), elType == "Visual" ? true : false, e.magnetic));
-            break;
-        default:
-    }
-});
-function getPointOfIntersection(startX1, startY1, endX1, endY1, startX2, startY2, endX2, endY2) {
-    var d = (startX1 - endX1) * (endY2 - startY2) - (startY1 - endY1) * (endX2 - startX2);
-    var da = (startX1 - startX2) * (endY2 - startY2) - (startY1 - startY2) * (endX2 - startX2);
-    var db = (startX1 - endX1) * (startY1 - startY2) - (startY1 - endY1) * (startX1 - startX2);
-
-    var ta = da / d;
-    var tb = db / d;
-
-    if (ta >= 0 && ta <= 1 && tb >= 0 && tb <= 1) {
-        var dx = startX1 + ta * (endX1 - startX1);
-        var dy = startY1 + ta * (endY1 - startY1);
-        return [dx, dy];
-    }
-
-    return [-100, -100];
-}
-function pointInPoly(poly, pointX, pointY) {
-    var i, j, c = false;
-    var polyCords = [[poly[0], poly[1]], [poly[2], poly[3]], [poly[4], poly[5]], [poly[6], poly[7]]];
-    for (i = 0, j = polyCords.length - 1; i < polyCords.length; j = i++) {
-
-        if (((polyCords[i][1] > pointY) != (polyCords[j][1] > pointY)) && (pointX < (polyCords[j][0] - polyCords[i][0]) * (pointY - polyCords[i][1]) / (polyCords[j][1] - polyCords[i][1]) + polyCords[i][0])) {
-            c = !c;
-        }
-
-    }
-
-    return c;
-}
+);
 var app = new Vue({
     el: "#app",
     data: {
         Vtest: null
     },
     methods: {
-        resize() {
-            let TestWidth = document.getElementById("testMain").clientWidth;
-            this.Vtest.renderer.resize(TestWidth, 500);
-        },
-        TestStart() {
-            this.Vtest = new PIXI.Application({ width: 100, height: 100, view: document.getElementById("MT1"), backgroundColor: 0xf0f0f0, antialias: true, })
-            const container = new PIXI.Container();
-            this.Vtest.stage.addChild(container);
-            Triggers.forEach((e) => {
-                container.addChild(e.graphics);
-            });
-            Elements.forEach((e) => {
-                container.addChild(e.sprite);
-            });
-            this.Vtest.ticker.add((delta) => {
-                Triggers.forEach((e) => {
-                    e.Draw();
-                    var elementcount = 0;
-                    Elements.forEach((el) => {
-                        if (e.magnetic) {
-                            if (pointInPoly(e.VectorArray, el.sprite.x, el.sprite.y)) {
-                                elementcount++;
-                            }
-                        }
-                        if (el.sprite.dragging) {
-                            if (e.magnetic && pointInPoly(e.VectorArray, el.sprite.hasposx, el.sprite.hasposy)) {
-                                el.sprite.OnTrigger = true;
-                                el.sprite.x = e.x;
-                                el.sprite.y = e.y;
-                            } else el.sprite.OnTrigger = false;
-                        }
-
-                    })
-                    e.elementcount = elementcount;
-                    // console.log(e.elementcount);
-                });
-            });
-
-
-
-
-        }
+   
+    
     },
     mounted() {
-        this.$nextTick(function () {
-            window.addEventListener('resize', this.resize);
-        })
-        this.TestStart();
-        this.resize();
+        this.Vtest = new VisualTest(Smap, document.getElementById("MT1"));
     }
 })
