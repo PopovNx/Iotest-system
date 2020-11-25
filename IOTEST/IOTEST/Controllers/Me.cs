f09b6e507edc286217dd4b80b6871214966f3bca
Andrey Popov <50965570+PopovDev@users.noreply.github.com>
2020-11-25 17:25:52 +0200

    Добавлен контроллер и view для /me

---
+++ IOTEST/IOTEST/Controllers/Me.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOTEST.Controllers
+{
+    [Route("/me")]
+    public class MeController : Controller
+    {
+        private IoContext Database;
+        public MeController(IoContext userContext)
+        {
+            Database = userContext;
+        }
+        public async Task<ActionResult> IndexAsync()
+        {
+            DataControl control = new DataControl(HttpContext.Request.Cookies);
+            if (!control.IsOk || !(await Database.Users.Where(x => x.Id == control.UserData.Id).AnyAsync())) { HttpContext.Response.Redirect("/login"); return View("Empty"); }
+
+            ViewData.Add("Title", $"Тест - Личный кабинет");
+            ViewData.Add("ParalaxOn", true);
+            ViewData.Add("CSS", new List<string> { "css/Test.css" });
+            ViewData.Add("JSU", new List<string> { "lib/Showdown/showdown.min.js", "lib/Pixijs/pixi.min.js", "js/VueComp.js" });
+            ViewData.Add("JSD", new List<string> { "js/Test.js" });
+            ViewData.Add("User", control.UserData);
+
+            return View("Me");
+        }
+
+    }
+}
