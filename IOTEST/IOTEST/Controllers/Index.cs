e8364580f25b8ff8cb811fbc6e7e812e2f63a94b
Andrey Popov <50965570+PopovDev@users.noreply.github.com>
2020-11-28 17:48:33 +0200

    Фикс 1

--- IOTEST/IOTEST/Controllers/Index.cs
+++ IOTEST/IOTEST/Controllers/Index.cs
@@ -22,12 +22,25 @@ namespace IOTEST.Controllers
             DataControl control = new DataControl(HttpContext.Request.Cookies);
             if (!control.IsOk || !(await Database.Users.Where(x => x.Id == control.UserData.Id).AnyAsync())) { HttpContext.Response.Redirect("/login"); return View("Empty"); }
             var UserHaveAcceptedTest = await Database.AcceptedLvls.AnyAsync(x => x.Email == control.UserData.Gmail);
+
             var Tests = new List<IoContext.AcceptedLvl>();
+            var NTests = new List<IoContext.Test>();
 
             if (UserHaveAcceptedTest)
                 Tests = await Database.AcceptedLvls.Where(x => x.Email == control.UserData.Gmail).ToListAsync();
 
+
+            var UserInGroups = (await Database.Groups.ToListAsync()).Any(x => x.Users.Contains(control.UserData.Gmail));
+            var TestsToProh = new List<string>();
+            if (UserInGroups)
+                TestsToProh = (await Database.Groups.ToListAsync()).Where(x => x.Users.Contains(control.UserData.Gmail)).Select(x => x.Key).ToList();
+
+            NTests = (await Database.Tests.ToListAsync());
+            NTests = NTests.Where(x => !Tests.Any(y => y.KEY == x.KEY)).ToList();
+
+
             ViewData.Add("Tests", Tests);
+            ViewData.Add("NTests", NTests);
             ViewData.Add("DataBase", Database);
             ViewData.Add("Control", control);
             ViewData.Add("UserHaveAcceptedTest", UserHaveAcceptedTest);
