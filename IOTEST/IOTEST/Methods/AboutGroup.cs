fd5b6e941699feae37bae4d9df803943726b17de
Andrey Popov <50965570+PopovDev@users.noreply.github.com>
2020-11-28 13:29:04 +0200

    Добавление метода получения информации о группе

---
+++ IOTEST/IOTEST/Methods/AboutGroup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+namespace IOTEST
+{
+    public partial class Methods
+    {
+        public class AboutGroup : IMethod
+        {
+            public async Task<string> Invoke(HttpContext context, IoContext DataBase, DataControl control)
+            {
+                if (!control.IsOk || !context.Request.Form.ContainsKey("Key")) return "error";
+                var Key = context.Request.Form["Key"].ToString();
+                var Group =await DataBase.Groups.FirstOrDefaultAsync(x => x.Key == Key);
+                return JsonConvert.SerializeObject(Group);
+            }
+        }
+    }
+}
--- IOTEST/IOTEST/wwwroot/js/Me.js
+++ IOTEST/IOTEST/wwwroot/js/Me.js
@@ -16,6 +16,14 @@
             Data.append('method', 'CreateGroup');
             Data.append('Name', this.NewGroupName);            
             axios.post('/method', Data).then(() => location = location);
+        },
+        AboutGroup(e) {
+            var Data = new FormData();
+            Data.append('method', 'AboutGroup');
+            Data.append('Key', e);
+            axios.post('/method', Data).then((x) => {
+                console.log(x.data);
+            });
         }
     },
     mounted() {
