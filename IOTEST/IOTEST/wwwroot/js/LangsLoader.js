1b067f9732de0a1a51669c349975d06d4ae13daa
Andrey Popov <andreypopov2628007@gmail.com>
2021-12-12 20:50:28 +0200

    LangAC

--- IOTEST/IOTEST/wwwroot/js/LangsLoader.js
+++ IOTEST/IOTEST/wwwroot/js/LangsLoader.js
@@ -1,3 +1,10 @@
-﻿import * as data from '../lang.json' assert { type: "json" };
+﻿import * as data from '../lang.json' assert {type: "json"};
+window.Lang = {};
 
-window.Lang = data.default;
+for (const key in data.default) {
+    if (key !== "version")
+        window.Lang[key] = data.default[key][window.LangSelected];
+    else
+        window.Lang[key] = data.default[key];
+}
+console.log(window.Lang)
