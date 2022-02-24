13b0c5db4319bc19fec21959931bc92e6574d50d
Andrey <andreypopov2628007@gmail.com>
2022-02-24 22:48:51 +0200

    zfg

---
+++ IOTEST/IOTEST/Controllers/Resources.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IOTEST.Controllers
+{
+    [Route("/resources")]
+    public class Resources : Controller 
+    {
+        private readonly IoContext _database;
+        public Resources(IoContext userContext) => _database = userContext;
+
+        public async Task<ActionResult> IndexAsync()
+        {
+            var control = new DataControl(HttpContext);
+            
+            if (!await control.Exist(_database))
+                return new RedirectResult("/login");
+            
+            var user = await _database.Users.FirstAsync(x => x.Id == control.UserData.Id);
+            
+            if (user.UserProf != IoContext.User.UserProfType.Teacher)
+                return new RedirectResult("/");
+
+
+            return View("Resources", user);
+        }
+    }
+}
---
+++ IOTEST/IOTEST/wwwroot/js/Resources.js
@@ -0,0 +1,32 @@
+﻿const app = new Vue({
+    el: "#app",
+    data: {
+        imgSelect: null,
+        imgFile: null,
+        imgBase64: "",
+        ResName:""
+    },
+    methods: {
+        LoadImg() {
+            const files = this.$refs.picInp.files;
+            if (files.length !== 1) return;
+            this.imgFile = files[0];
+        },
+        AddRes(){
+            const tx = this.ResName;
+   
+        }
+    },
+    watch: {
+        imgFile: function (e) {
+            
+            const reader = new FileReader();
+            reader.onload=(e)=> this.imgBase64 = e.target.result
+            reader.readAsDataURL(this.imgFile);
+        }
+    },
+    computed: {},
+    async mounted() {
+
+    }
+});
