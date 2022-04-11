c99c26122a76405b94716e211e2aba666e79eaae
Akure <75520842+AlexWaisley@users.noreply.github.com>
2022-04-11 21:04:52 +0300

    ニャー

--- IOTEST/IOTEST/wwwroot/js/LangsLoader.js
+++ IOTEST/IOTEST/wwwroot/js/LangsLoader.js
@@ -625,10 +625,20 @@
         "Добавить",
         "Add"
     ],
-    "TestCreatorResourcesTransparency": [
+    "TestCreatorResourcesOnlyVisible": [
         "Тільки видимі:",
         "Только видимые:",
         "Only visible:"
+    ],
+    "TestCreatorAnimationBlur": [
+        "Розмиття:",
+        "Размытие:",
+        "Blur:"
+    ],
+    "TestCreatorAnimationTransparency": [
+        "Прозорість",
+        "Прозрачность",
+        "Transparency"
     ],
     "TestCreatorResourcesCountable": [
         "Оцінюється:",
